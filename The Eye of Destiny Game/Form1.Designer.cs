9a4ff907d5b3e25b662fa52b5ef1a804b7c6c20d
Administrator <Administrator@DESKTOP-BUGKGO7>
2025-11-05 20:20:01 +0800

    Add project files.

---
+++ The Eye of Destiny Game/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace The_Eye_of_Destiny_Game
+{
+    partial class TheEyeofDestiny
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TheEyeofDestiny));
+            this.submitButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.submitButton.BackColor = System.Drawing.Color.Plum;
+            this.submitButton.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.Color.DarkViolet;
+            this.submitButton.Location = new System.Drawing.Point(354, 464);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(90, 34);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Tell Me!";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.questionBox.BackColor = System.Drawing.Color.Thistle;
+            this.questionBox.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionBox.ForeColor = System.Drawing.Color.Purple;
+            this.questionBox.Location = new System.Drawing.Point(179, 417);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(444, 30);
+            this.questionBox.TabIndex = 1;
+            this.questionBox.Text = "type your question here...";
+            this.questionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.questionBox.TextChanged += new System.EventHandler(this.questionBox_TextChanged);
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Papyrus", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.answerLabel.Location = new System.Drawing.Point(234, 161);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(342, 165);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.answerLabel.Click += new System.EventHandler(this.answerLabel_Click);
+            // 
+            // title
+            // 
+            this.title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.ForeColor = System.Drawing.Color.LavenderBlush;
+            this.title.Location = new System.Drawing.Point(266, 36);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(271, 87);
+            this.title.TabIndex = 3;
+            this.title.Text = "The Eye of Destiny";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetButton.BackColor = System.Drawing.Color.Thistle;
+            this.resetButton.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.resetButton.Location = new System.Drawing.Point(666, 444);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(84, 37);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // TheEyeofDestiny
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.submitButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TheEyeofDestiny";
+            this.Text = "zzzz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.Label answerLabel;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ The Eye of Destiny Game/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace The_Eye_of_Destiny_Game
+{
+    public partial class TheEyeofDestiny : Form
+    {
+        Random random = new Random();
+        List<string> answers = new List<string>();
+        public TheEyeofDestiny()
+        {
+            InitializeComponent();
+            // Yes answers
+            answers.Add("Verily, it shall come to pass.");
+            answers.Add("By the king's honor, aye indeed.");
+            answers.Add("The stars decree it so.");
+            answers.Add("A most certain truth, fair one.");
+            answers.Add("Yea, destiny smiles upon thee.");
+            answers.Add("So it is written in the old scrolls.");
+            answers.Add("By sword and spell, it shall be done.");
+            answers.Add("The omens bode well for thy wish.");
+            // Maybe answers
+            answers.Add("Ask again when the moon is full.");
+            answers.Add("The fates whisper, yet their words are unclear.");
+            answers.Add("In time, the truth shall reveal itself.");
+            answers.Add("Even the wisest sage cannot foretell this.");
+            answers.Add("The runes speak in riddles this night.");
+            answers.Add("Perchance, though shadows yet linger.");
+            //Negative answers
+            answers.Add("Nay, such fortune is not thine.");
+            answers.Add("By the gods, it shall not be.");
+            answers.Add("The stars turn cold against thee.");
+            answers.Add("Abandon hope, for it is not destined.");
+            answers.Add("The answer lieth dark and grim");
+            answers.Add("Thy wish falleth upon deaf heavens");
+            answers.Add("Fate refuseth thy plea.");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            int index = random.Next(answers.Count);
+            answerLabel.Text = answers[index];
+        }
+
+        private void questionBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void answerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            questionBox.Text = "";
+            answerLabel.Text = "";
+        }
+    }
+}
